83af7f06c23b9f70d08b8c5e667deed16beae89f
Th3mis25 <vmprpg@outlook.com>
2025-10-15 10:05:25 -0600

    Delete seguimiento_cargas_pwa/formatHeaderLabel.test.js

--- seguimiento_cargas_pwa/formatHeaderLabel.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const { formatHeaderLabel } = require('./app.js');
-
-assert.strictEqual(formatHeaderLabel('TR-MX'), 'TR-MX');
-assert.strictEqual(formatHeaderLabel('TR-USA'), 'TR-USA');
-assert.strictEqual(formatHeaderLabel('cliente'), 'Cliente');
-assert.strictEqual(formatHeaderLabel(' cita carga '), 'Cita carga');
-
-console.log('formatHeaderLabel tests passed.');
